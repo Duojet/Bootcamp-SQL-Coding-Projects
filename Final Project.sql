e0c606245fb27da41d27239ea841dd1155df17ea
Duojet <jordanaudio@mac.com>
2019-03-06 16:01:17 -0800

    Update Final Project.sql

--- Final Project.sql
+++ Final Project.sql
@@ -10,7 +10,6 @@ CREATE TABLE libraryBranch (
 	branchAddress VARCHAR(50) NOT NULL
 );
 
-
 CREATE TABLE publisher (
 	publisherName VARCHAR(30) PRIMARY KEY NOT NULL,
 	publisherAddress VARCHAR(50) NOT NULL,
@@ -20,7 +19,7 @@ CREATE TABLE publisher (
 CREATE TABLE books (
 	bookID INT PRIMARY KEY NOT NULL IDENTITY (1,1),
 	bookTitle VARCHAR(50) NOT NULL,
-	publisherName varchar(30) FOREIGN KEY REFERENCES publisher(publisherName) NOT NULL
+	publisherName VARCHAR(30) FOREIGN KEY REFERENCES publisher(publisherName) NOT NULL
 );
 
 
@@ -39,7 +38,7 @@ CREATE TABLE bookCopies (
 CREATE TABLE borrower (
 	cardNum VARCHAR(30) PRIMARY KEY NOT NULL,
 	borrowerName VARCHAR(30) NOT NULL,
-	borrowerAddress VARCHAR(30) NOT NULL,
+	borrowerAddress VARCHAR(50) NOT NULL,
 	borrowerPhone VARCHAR(30) NOT NULL
 );
 
@@ -64,11 +63,10 @@ INSERT INTO libraryBranch
 	;
 
 
-
 INSERT INTO publisher
 	(publisherName, publisherAddress, publisherPhone)
 	VALUES
-	('G.P. PUTNAM''S SONS', '45 Lexington Ave New York, NY  10002', '454-555-5656'),
+	('G.P. PUTNAMS SONS', '45 Lexington Ave New York, NY  10002', '454-555-5656'),
 	('CELADON BOOKS', '5256 SE Floss Blve Albany, NY  10152', '789-456-6123'),
 	('LAKE UNION PUBLISHING', '24 Hendrix Ave  Tacoma, WA  99856', '123-456-7893'),
 	('POTTERMORE PUBLISHING', 'PO Box 7828, London W1A 4GE, UK', '+044 5556 542565'),
@@ -79,7 +77,8 @@ INSERT INTO publisher
 	('THOMAS & MERCER', '4564 N 10th blvd Branson, MO  45662', '456-888-9969'),
 	('MONTLAKE ROMANCE', '454 Fleet St. North Tombridge, SI  45689', '778-989-6547'),
 	('WILLIAM MORROW', '4585 Iam Bored LN Town Center, MA  12302', '484-651-9876'),
-	('HARPERIMPULSE', '5 Fortnight Row  Blantington, OL  12587', '789-965-3214')
+	('HARPERIMPULSE', '5 Fortnight Row  Blantington, OL  12587', '789-965-3214'),
+	('Henry Holt & Co', '258 Publisher St Goober, OH  45215', '546-654-5465')
 	;
 
 
@@ -87,7 +86,7 @@ INSERT INTO publisher
 INSERT INTO books
 	(bookTitle, publisherName)
 	VALUES
-	('Where the Crawdads Sing', 'G.P. PUTNAM''S SONS'),
+	('Where the Crawdads Sing', 'G.P. PUTNAMS SONS'),
 	('The Silent Patient', 'CELADON BOOKS'),
 	('Where the Forest Meets the Stars', 'LAKE UNION PUBLISHING'),
 	('Surprise Delivery','R.R. Banks'),
@@ -111,43 +110,144 @@ INSERT INTO books
 
 
 INSERT INTO bookAuthors
-	(authorName)
+	(bookID,authorName)
 	VALUES
-	('DELIA OWENS'),
-	('ALEX MICHAELIDES'),
-	('GLENDY VANDERAH'),
-	('R.R. BANKS'),
-	('J.K. ROWLING'),
-	('AMY HARMON'),
-	('JENNIFER GRAESER DORNBUSH'),
-	('GAIL HONEYMAN'),
-	('MARK SULLIVAN'),
-	('STEVEN KONKOLY'),
-	('BARRY EISLER'),
-	('JANE HEALEY'),
-	('HEATHER MORRIS'),
-	('ADRIANA LOCKE'),
-	('KEN FOLLETT'),
-	('LUCINDA BERRY'),
-	('RHYS BOWEN'),
-	('EDWARD MARRIOT'),
-	('STEPHEN KING')
+	(1,'DELIA OWENS'),
+	(2,'ALEX MICHAELIDES'),
+	(3,'GLENDY VANDERAH'),
+	(4,'R.R. BANKS'),
+	(5,'J.K. ROWLING'),
+	(6,'AMY HARMON'),
+	(7,'JENNIFER GRAESER DORNBUSH'),
+	(8,'GAIL HONEYMAN'),
+	(9,'MARK SULLIVAN'),
+	(10,'STEVEN KONKOLY'),
+	(11,'BARRY EISLER'),
+	(12,'JANE HEALEY'),
+	(13,'HEATHER MORRIS'),
+	(14,'ADRIANA LOCKE'),
+	(15,'KEN FOLLETT'),
+	(16,'LUCINDA BERRY'),
+	(17,'RHYS BOWEN'),
+	(18,'EDWARD MARRIOT'),
+	(19,'STEPHEN KING'),
+	(20,'STEPHEN KING')
 	;
 
 
 
 
+INSERT INTO borrower
+	(cardNum, borrowerName, borrowerAddress, borrowerPhone)
+	VALUES
+	('123sdf', 'Joe Bob', '1234 SE Burbon St. Portand, OR  97951', '503-123-4567'),
+	('123qwr', 'Jenny Todd', '1475 SE Tooms Rd. Portand, OR  97615', '503-123-8756'),
+	('123asd', 'Bilbo Baggins', '1010 N Shire Ct. Portand, OR  97132', '503-258-9632'),
+	('123fgh', 'Teddy Ruxpin', '123 SE Kissmybutt Rd. Portand, OR  97875', '503-741-8520'),
+	('123sdg', 'Tony Toni', '156 SE Burbon St. Portand, OR  97951', '503-123-5267'),
+	('123qwj', 'Gosh Golly', '1475 SE Bontomps Rd. Portand, OR  97315', '503-644-8756'),
+	('123ask', 'Dude Stop', '1013 N Shire Ct. Portand, OR  97132', '503-258-6464'),
+	('123fgl', 'Jaun Moore', '5858 117 St. Portand, OR  97214', '503-741-6920')
+	;
+
 INSERT INTO bookLoans
+	(bookID, branchID, cardNum, dateOut)
+	VALUES
+	(1, 1, '123sdf', 2019-12-12),
+	(16, 1, '123sdf', 2019-12-12)  
+
+
+
+INSERT INTO bookCopies
+	(bookID, branchID, NumberOfCopies)
+	VALUES
+	(1, 1, 15),
+	(1, 2, 13),
+	(1, 3, 12),
+	(1, 4, 5),
+	(2, 1, 15),
+	(2, 2, 13),
+	(2, 3, 12),
+	(2, 4, 5),
+	(3, 1, 15),
+	(3, 2, 13),
+	(3, 3, 12),
+	(3, 4, 5),
+	(4, 1, 15),
+	(4, 2, 13),
+	(4, 3, 12),
+	(4, 4, 5),
+	(5, 1, 15),
+	(5, 2, 13),
+	(5, 3, 12),
+	(5, 4, 5),
+	(6, 1, 15),
+	(6, 2, 13),
+	(6, 3, 12),
+	(6, 4, 5),
+	(7, 1, 15),
+	(7, 2, 13),
+	(7, 3, 12),
+	(7, 4, 5),
+	(8, 1, 15),
+	(8, 2, 13),
+	(8, 3, 12),
+	(8, 4, 5),
+	(9, 1, 15),
+	(9, 2, 13),
+	(9, 3, 12),
+	(9, 4, 5),
+	(10, 1, 15),
+	(10, 2, 13),
+	(10, 3, 12),
+	(10, 4, 5),
+	(11, 1, 2),
+	(11, 2, 2),
+	(11, 3, 11),
+	(11, 4, 5),
+	(12, 1, 15),
+	(13, 2, 13),
+	(14, 3, 12),
+	(14, 4, 5),
+	(15, 1, 15),
+	(15, 2, 13),
+	(15, 3, 12),
+	(15, 4, 5),
+	(15, 1, 15),
+	(15, 2, 13),
+	(15, 3, 12),
+	(15, 4, 5),
+	(15, 1, 15),
+	(15, 2, 13),
+	(15, 3, 12),
+	(15, 4, 5),
+	(15, 1, 15),
+	(15, 2, 13),
+	(15, 3, 12),
+	(15, 4, 5),
+	(19, 1, 15),
+	(19, 2, 13),
+	(19, 3, 12),
+	(19, 4, 5),
+	(20, 1, 15),
+	(20, 2, 13),
+	(20, 3, 12),
+	(20, 4, 5)
+;
+
+
+
+
+
 	
 
 
 
 
 
-INSERT INTO borrower
 
 
-*/
+
 
 
 
